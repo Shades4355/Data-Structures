9816e55504c1c06dcec7cde573135709a9d12fde
Shades <shades4355@gmail.com>
2024-07-09 14:02:07 -0400

    WIP - class time

--- assignment-02/Driver.java
+++ assignment-02/Driver.java
@@ -2,7 +2,7 @@
 // Written by:  Shades Meyers
 // Description: Client-side program for organizing News Articles 
 // Challenges:  
-// Time Spent:  1 min
+// Time Spent:  10 min + 
 //
 // Revision history:
 // Date:        By:     Action:
@@ -11,8 +11,25 @@
 // 2024-July-09 SM      Added constructor
 
 
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello!"); // TODO: delete
+        Scanner input = new Scanner(System.in);
+        String answer;
+        boolean addingArticles = true;
+
+        System.out.println("Welcome!");
+
+        while (addingArticles){
+            System.out.println("Would you like to add an Article? (y/n)");
+            answer = input.nextLine();
+
+            if (answer.toLowerCase() == "n") {
+                addingArticles = false;
+            }
+        }
+        input.close();
+        System.exit(0);
     }
 } // End Program
