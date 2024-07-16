e4ad26ecdea89540cd1734ffe0a3749831a65e07
Shades <shades4355@gmail.com>
2024-07-16 18:52:32 -0400

    updated DLL ~ renamed to DLL_v2 to avoid naming isues ~ added more comments ~ fixed some logic mistakes
    * updated Driver based on DLL_v2 changes
    
    * removed commented out code from DLL_v2 and Driver

---
+++ in-class/in-class-04_v2/DLL_v2.java
@@ -0,0 +1,176 @@
+// File name:   DLL_v2.java
+// Written by:  Shades Meyers
+// Description: A Double Linked List of DLLNode<E> nodes
+//
+// Revision history:
+// Date:        By:     Action:
+// -------------------------------
+// 2024-July-16 SM      Updated
+
+
+import java.util.Objects;
+import java.util.function.Consumer;
+
+public class DLL_v2<E> {
+    // variables
+    private int size;
+    private DLLNode<E> header;
+    private DLLNode<E> trailer;
+
+    // Constructors
+    DLL_v2(DLLNode<E> node) {
+        this.size = 1;
+        this.header = node;
+        this.trailer = node;
+    }
+    DLL_v2() {
+        this.size = 0;
+        this.header = null;
+        this.trailer = null;
+    }
+
+    // Accessors & Mutators
+    // Size
+    public int size() {
+        return this.size;
+    }
+
+    // is empty?
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    // Insertions
+    public void add(E element) {
+        // calls addLast
+        this.addLast(element);
+    }
+    public void addLast(E element) {
+        DLLNode<E> node = new DLLNode<E>(element);
+        if (this.header == null) { // if list is empty...
+            this.header = node;
+            this.trailer = node;
+        } else { // if list has at least 1 node...
+            this.trailer.setNextNode(node);
+            this.trailer = node;
+        }
+        this.size += 1;
+    }
+    public void addFirst(E element) {
+        DLLNode<E> node = new DLLNode<E>(element);
+        node.setNextNode(this.header);
+        this.header = node;
+
+        if (this.trailer == null) { // if list was empty before addition...
+            this.trailer = node;
+        }
+
+        this.size += 1;
+    }
+
+    // Get
+    public E get(int index) { // get element at given index
+        DLLNode<E> currentNode = this.header;
+        if (index < this.size && index >= 0) {
+            for (int i = 1; i <= index; i++) {
+                currentNode = currentNode.getNextNode();
+            }
+        } else {
+            return null;
+        }
+        return currentNode.getElement();
+    }
+    public E first() { return this.header.getElement(); }
+    public E last() { return this.trailer.getElement(); }
+
+    // add between
+    public void addBetween(E element, DLLNode<E> before, DLLNode<E> after) {
+        DLLNode<E> newNode = new DLLNode<E>(element);
+        before.setNextNode(newNode);
+        after.setPrevNode(newNode);
+
+        newNode.setPrevNode(before);
+        newNode.setNextNode(after);
+    }
+
+    // removal
+    public E removeFirst() {
+        if (this.size > 0){ // if there is a node to remove...
+            DLLNode<E> toBeRemoved = this.header;
+            if (toBeRemoved.getNextNode() != null) {
+                this.header = toBeRemoved.getNextNode();
+                size -= 1;
+            } else { // if there's only 1 node...
+                this.header = null;
+                this.trailer = null;
+                this.size = 0;
+            }
+
+            return toBeRemoved.getElement();
+        } else { // if there's no nodes in list...
+            return null;
+        }
+    }
+
+    public E removeLast() {
+        if (this.size > 0) { // If there's a node to remove...
+            DLLNode<E> oldTrailer = this.trailer;
+            if (oldTrailer.getPrevNode() != null) { // if there's more than 1 node in list...
+                DLLNode<E> node = this.trailer.getPrevNode();
+
+                node.setNextNode(null);
+                this.trailer = node;
+                this.size -= 1;
+            } else { // if there's only 1 node in list...
+                this.header = null;
+                this.trailer = null;
+                this.size = 0;
+            }
+            return oldTrailer.getElement();
+        } else { // if there's no nodes in list...
+            return null;
+        }
+    }
+
+    // remove a specific node
+    public E remove(DLLNode<E> node) {
+        if (this.isEmpty()) { // if the list is empty...
+            return null;
+        }
+
+        // as long as there's 1 or more nodes in list...
+        if (node.getPrevNode() != null && node.getNextNode() != null) {
+            // if node has a before and after...
+            node.getPrevNode().setNextNode(node.getNextNode());
+            node.getNextNode().setPrevNode(node.getPrevNode());
+        } else if (node.getPrevNode() == null && node.getNextNode() != null) { // if node is in the header...
+            this.removeFirst();
+        } else { // if node is trailer...
+            this.removeLast();
+        }
+
+        return node.getElement();
+    }
+
+    // iterable
+    // .forEac() accepts lambda expressions
+    // ex: list.forEach((n) -> System.out.println(n));
+    public void forEach(Consumer<? super E> action) throws ArrayIndexOutOfBoundsException {
+        try{
+            Objects.requireNonNull(action);
+            DLLNode<E> currentNode = this.header;
+
+            if (this.size > 0) {
+                while (currentNode.hasNext()) {
+                    action.accept(currentNode.getElement());
+                    currentNode = currentNode.getNextNode();
+                }
+                action.accept(currentNode.getElement());
+            } else {
+                throw new ArrayIndexOutOfBoundsException("List is empty");
+            }
+        } catch (NullPointerException e) {
+            System.err.println(e);
+        }
+    }
+} // end program
--- in-class/in-class-04_v2/Driver.java
+++ in-class/in-class-04_v2/Driver.java
@@ -1,23 +1,26 @@
-import java.lang.reflect.InvocationTargetException;
+// File name:   Driver.java
+// Written by:  Shades Meyers
+// Description: A Tester for DLL_v2 and DLLNode classes
+//
+// Revision history:
+// Date:        By:     Action:
+// -------------------------------
+// 2024-July-16 SM      Updated
+
 
 public class Driver {
     public static void main(String[] args) {
-        DLL<String> list1 = new DLL<String>();
-        DLL<String> list2 = new DLL<String>(new DLLNode<String>("node 1"));
-
-        DLLNode<String> node2 = new DLLNode<String>("node 2");
-        DLLNode<String> node3 = new DLLNode<String>("node 3");
-        DLLNode<String> node4 = new DLLNode<String>("node 4");
-        DLLNode<String> node5 = new DLLNode<String>("node 5");
+        DLL_v2<String> list1 = new DLL_v2<String>();
+        DLL_v2<String> list2 = new DLL_v2<String>(new DLLNode<String>("original node"));
 
         // list 1
         System.out.println("List 1 is empty: " + list1.isEmpty());
 
-        list1.addFirst(node2);
-        list1.add(node3);
+        list1.addFirst("node 1");
+        list1.add("node 2");
         System.out.println("2 Nodes added");
         
-        System.out.println("List 1 is empty: " + list1.isEmpty());
+        System.out.println("\nList 1 is empty: " + list1.isEmpty());
         System.out.println("Fist element is: " + list1.first());
         System.out.println("Last element is: " + list1.last());
         System.out.println("List size: " + list1.size());
@@ -27,11 +30,11 @@ public class Driver {
         // list 2
         System.out.println("List 2 is empty: " + list2.isEmpty());
 
-        list2.addFirst(node4);
-        list2.addLast(node5);
+        list2.addFirst("first node");
+        list2.addLast("last node");
         System.out.println("2 Nodes added");
 
-        System.out.println("List 1 is empty: " + list2.isEmpty());
+        System.out.println("\nList 2 is empty: " + list2.isEmpty());
         System.out.println("Fist element is: " + list2.first());
         System.out.println("Second element is: " + list2.get(1));
         System.out.println("Last element is: " + list2.last());
@@ -40,34 +43,6 @@ public class Driver {
 
         // forEach
         System.out.println("\nforEach test:");
-        list2.forEach((n) -> System.out.println(n));    
-
-        // error: Can only iterate over an array or an instance of java.lang.Iterable
-        // for (DLLNode<String> node : list2) {
-        //     System.out.println(node);
-        // }
-
-        // my first attempt at iteration
-        // System.out.println("\niterator2:");
-        // DLLNode<String>[] iterable = list2.iterator2();
-        // for (DLLNode<String> node : iterable) {
-        // System.out.println(node.getElement());
-        // }
-
-        // written with SI's help (90% SI; 10% me)
-        System.out.println("\nIterable test:");
-        Object iterableList = list2.iterator();
-        try {
-            while ((boolean) iterableList.getClass().getMethod("hasNext").invoke(iterableList)) {
-                String string = (String) iterableList.getClass().getMethod("next").invoke(iterableList);
-                System.out.println(string);
-            }
-        } catch (NoSuchMethodException e) {
-            System.err.println(e);
-        } catch (IllegalAccessException j) {
-            System.err.println(j);
-        } catch (InvocationTargetException k) {
-            System.err.println(k);
-        }
+        list2.forEach((n) -> System.out.println("\t" + n));    
     } // end main
 } // end program
