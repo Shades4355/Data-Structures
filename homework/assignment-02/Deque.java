1cc92faab51db37f35f7786036853fed397a09f6
Shades <shades4355@gmail.com>
2024-07-17 14:51:06 -0400

    outlined necessary variables and functions for Deque

--- homework/assignment-02/Deque.java
+++ homework/assignment-02/Deque.java
@@ -11,5 +11,17 @@
 
 
 public class Deque {
+    // variables
+    // private int sz;
     
+    // Methods
+    // public int size()
+    // public boolean isEmpty()
+    // public E first()
+    // public E last()
+    // public void addFirst(E element)
+    // public void addLast(E element)
+    // public E removeFirst()
+    // public E removeLast()
+
 }
