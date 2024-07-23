88d5798f985d2907c022c6436e49e9f70e98cdd5
Shades <shades4355@gmail.com>
2024-07-23 18:09:33 -0400

    updated DLL and AList based on in-class comments

--- homework/assignment03/AList.java
+++ homework/assignment03/AList.java
@@ -4,8 +4,8 @@
 // Challenges:  add(index, element) failing for non-obvious
 //                  reasons. Something along the action chain 
 //                  is returning a null value when it shouldn't
-//                  (Had >= instead of <= in a for loop; oops)
-// Time Spent:  3 h 19 min
+//                  (solution: Had >= instead of <= in a for loop; oops)
+// Time Spent:  3 h 26 min
 //
 // Revision history:
 // Date:        By:     Action:
@@ -18,125 +18,38 @@
 //                      First run of Integer test file
 //                      Passed Integer test after some adjustments
 //                      First run of Person test file - passes
+// 2024-July-23 SM      Moved several Methods into new DLL file
+//                      Extended DLL<E>
+//                      Changed several instances of "this" to "super"
+//                          for readability
 
 
 import java.util.Objects;
 import java.util.function.Consumer;
 
-public class AList<E> implements AListADT<E> {
-    // private Node class
-    private static class Node<E> {
-        // variables
-        private E element;
-        private Node<E> nextNode;
-        private Node<E> prevNode;
-
-        // Constructor
-        public Node(E element, Node<E> prev, Node<E> next) {
-            this.element = element;
-            this.prevNode = prev;
-            this.nextNode = next;
-        }
-
-        // Accessors & Mutators
-        // Elements
-        public E getElement() throws IllegalStateException {
-            if (nextNode == null) {
-                throw new IllegalStateException("Position no longer valid");
-            }
-            return this.element;
-        }
-        public void setElement(E element) {
-            this.element = element;
-        }
-
-        // Return the next node
-        public Node<E> getNext() {
-            return this.nextNode;
-        }
-        public void setNext(Node<E> nextNode) {
-            this.nextNode = nextNode;
-        }
-
-        // Return the previous node
-        public Node<E> getPrev() {
-            return this.prevNode;
-        }
-        public void setPrev(Node<E> prevNode) {
-            this.prevNode = prevNode;
-        }
-
-        // Returns true if the node in question has a next node
-        public boolean hasNext() {
-            return this.getNext() != null;
-        }
-
-        // Implementation of a toString() method
-        @Override
-        public String toString() {
-            StringBuilder string = new StringBuilder();
-            string.append(this.getElement());
-            
-            return new String(string);
-        }
-    } // end Node private class
-    
-    // Variables
-    private int sz;
-    private Node<E> header;
-    private Node<E> trailer;
+public class AList<E> extends DLL<E> implements AListADT<E> {
 
     // Constructors
     AList() {
-        header = new Node<E>(null, null, null);
-        trailer = new Node<E>(null, header, null);
-        header.setNext(trailer);
-        this.sz = 0;
+        super();
     }
     AList(E element) {
-        header = new Node<E>(null, null, null);
-        trailer = new Node<E>(null, null, null);
-        Node<E> node = new Node<E>(element, header, trailer);
-        header.setNext(node);
-        trailer.setPrev(node);
-        this.sz = 1;
+        super(element);
     }
 
     // Methods
-    // Return size of list, excluding sentinels 
-    public int size() { return this.sz; }
-
-    // Returns true if this list contains no elements.
-    public boolean isEmpty() { return this.sz == 0; }
-
     // Insertion methods
     // Appends the specified element to the end of this list. (at the tail end.)
     // Returns: true
     public boolean add(E element) {
-        this.addLast(element);
+        super.addLast(element);
 
         return true;
-    }
-    // Appends the specified element to the beginning of this list.
-    public void addFirst(E element) {
-        this.addBetween(element, header, header.getNext());
-    }
-    // Appends the specified element to the end of this list. 
-    public void addLast(E element) {
-        this.addBetween(element, trailer.getPrev(), trailer);
-    }
-    // Add a new node of 'element' between two known nodes
-    private void addBetween(E element, Node<E> before, Node<E> after) {
-        Node<E> newNode = new Node<E>(element, before, after);
-        before.setNext(newNode);
-        after.setPrev(newNode);
-
-        this.sz++;
     }
     // Inserts the specified element at the specified position in this list.
     public void add(int index, E element) throws IndexOutOfBoundsException {
         Node<E> node = findNode(index);
-        this.addBetween(element, node.getPrev(), node);
+        super.addBetween(element, node.getPrev(), node);
     }
 
     // Peek methods
@@ -144,14 +57,6 @@ public class AList<E> implements AListADT<E> {
     public E get(int index) throws IndexOutOfBoundsException {
         Node<E> node = findNode(index);
         return node.getElement();
-    }
-    // Returns the first element in list.
-    public E first() {
-        return this.header.getNext().getElement();
-    }
-    // Return the last element in list.
-    public E last() {
-        return this.trailer.getPrev().getElement();
     }
 
     // Replaces the element at the specified position in this list with the
@@ -168,8 +73,8 @@ public class AList<E> implements AListADT<E> {
     // Returns the index of the first occurrence of the specified element in this
     // list, or -1 if this list does not contain the element.
     public int indexOf(E element) {
-        Node<E> currentNode = this.header;
-        for (int i = 0; i < this.sz; i++) {
+        Node<E> currentNode = super.header;
+        for (int i = 0; i < super.size(); i++) {
             currentNode = currentNode.getNext();
             if (currentNode.getElement() == element) {
                 return i;
@@ -180,8 +85,8 @@ public class AList<E> implements AListADT<E> {
 
     // Returns true if this list contains the specified element, false otherwise
     public boolean contains(E element) {
-        Node<E> currentNode = this.header;
-        for (int i = 0; i < this.sz; i++) {
+        Node<E> currentNode = super.header;
+        for (int i = 0; i < super.size(); i++) {
             currentNode = currentNode.getNext();
             if (currentNode.getElement() == element) {
                 return true;
@@ -195,60 +100,33 @@ public class AList<E> implements AListADT<E> {
     // Returns: the element that was removed from the list
     public E remove(int index) throws IndexOutOfBoundsException {
         Node<E> node = findNode(index);
-        return this.remove(node);
+        return super.remove(node);
     }
-    // Removes a specified node
-    //Returns: the element of the removed node
-    private E remove(Node<E> node) {
-        E nodeElement = node.getElement();
-        node.getNext().setPrev(node.getPrev());
-        node.getPrev().setNext(node.getNext());
-        node.setNext(null);
-        node.setPrev(null);
-        this.sz--;
-
-        return nodeElement;
-    }
+    
     // Removes the first occurrence of the specified element from this list, if it
     // is present.
     // Returns: true if this list contained the specified element, false otherwise
     public boolean remove(E element) {
-        Node<E> currentNode = this.header;
-        for (int i = 0; i < this.sz; i++) {
+        Node<E> currentNode = super.header;
+        for (int i = 0; i < super.size(); i++) {
             currentNode = currentNode.getNext();
             if (currentNode.getElement() == element) {
-                remove(currentNode);
+                super.remove(currentNode);
                 return true;
             }
         }
         return false;
     }
-    // Removes first node in the list
-    // Returns: the removed node's element
-    public E removeFirst() {
-        if (this.sz > 0) {
-            return remove(this.header.getNext());
-        }
-        return null;
-    }
-    // Removes the last node in the list
-    // Returns: the removed node's element
-    public E removeLast() {
-        if (this.sz > 0) {
-            return remove(this.trailer.getPrev());
-        }
-        return null;
-    }
-
+    
     // Returns a string containing all the elements of the list that matches the
     // output format of the spec
     public String toString() {
         StringBuilder string = new StringBuilder();
         string.append("The AList:{");
-        if (this.sz > 0) {
+        if (super.size() > 0) {
             this.forEach((n) -> {
                 string.append(n.getElement());
-                if (n.getNext() != this.trailer) {
+                if (n.getNext() != super.trailer) {
                     string.append(",");
                 }
             });
@@ -264,7 +142,7 @@ public class AList<E> implements AListADT<E> {
     // at index 0)
     private Node<E> findNode(int index) throws IndexOutOfBoundsException{
         if (validateIndex(index)) {
-            Node<E> currentNode = this.header;
+            Node<E> currentNode = super.header;
             for (int i = 0; i <= index; i++) {
                 currentNode = currentNode.getNext();
             }
@@ -276,7 +154,7 @@ public class AList<E> implements AListADT<E> {
     // Validates that a given index is within the bounds of the list
     // Returns: true or throws an exception
     private boolean validateIndex(int index) throws IndexOutOfBoundsException {
-        if (index >= 0 && index < this.sz) {
+        if (index >= 0 && index < super.size()) {
             return true;
         }
         throw new IndexOutOfBoundsException("Index out of bounds.");
@@ -288,9 +166,9 @@ public class AList<E> implements AListADT<E> {
     public void forEach(Consumer<? super Node<E>> action) throws ArrayIndexOutOfBoundsException {
         try{
             Objects.requireNonNull(action);
-            Node<E> currentNode = this.header.getNext();
+            Node<E> currentNode = super.header.getNext();
 
-            if (this.sz > 0) {
+            if (super.size() > 0) {
                 while (currentNode.hasNext()) {
                     action.accept(currentNode);
                     currentNode = currentNode.getNext();
