3b79882388817847d595d742d3fda206f50d60da
Shades <shades4355@gmail.com>
2024-07-25 23:55:01 -0400

    started a driver class to test Tree class's functionality

---
+++ homework/assignment04/TestDriver.java
@@ -0,0 +1,35 @@
+// File name: TestDriver.java
+// Written by: Shades Meyers
+// Description: A Driver file for testing Tree.java
+// Challenges:
+// Time Spent: 15 minutes
+//
+// Revision history:
+// Date:        By:     Action:
+// -------------------------------
+// 2024-July-25 SM      File created
+
+
+public class TestDriver {
+    public static void main(String[] args) {
+        Tree<String, Integer> inventory = new Tree<String, Integer>();
+
+        inventory.add(new Pairs<String, Integer>("Healing Potions", 3));
+
+        System.out.println(inventory);
+        // expected outcome:
+        // {{Healing Potions: 3}}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+        inventory.add(new Pairs<String,Integer>("Arrows", 10));
+        System.out.println(inventory);
+        // expected outcome:
+        // {{Arrows: 10}, {Healing Potions: 3}}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+        inventory.add("Bombs", 4);
+        System.out.println(inventory);
+        // expected outcome:
+        // {{Arrows: 10}, {Bombs: 4}, {Healing Potions: 3}}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+        
+    } // end main method
+} // end program
