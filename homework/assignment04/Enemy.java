f167d3d2a537cb182ee05719b3c8f33c881ebae7
Shades <shades4355@gmail.com>
2024-07-27 23:17:33 -0400

    empty Enemy and Hero classes created

---
+++ homework/assignment04/Enemy.java
@@ -0,0 +1,20 @@
+// File name:   Enemy.java
+// Written by:  Shades Meyers
+// Description: A template for Enemies
+// Challenges:  
+// Time Spent:  1 minutes + ( - )
+//
+// Revision history:
+// Date:        By:     Action:
+// -------------------------------
+// 2024-July-27 SM      File created
+
+
+public class Enemy {
+    // Variables
+
+    // Constructors
+
+    // Methods
+
+}
---
+++ homework/assignment04/Hero.java
@@ -0,0 +1,20 @@
+// File name:   Hero.java
+// Written by:  Shades Meyers
+// Description: A template for the Player
+// Challenges:  
+// Time Spent:  1 minutes + ( - )
+//
+// Revision history:
+// Date:        By:     Action:
+// -------------------------------
+// 2024-July-27 SM      File created
+
+
+public class Hero {
+    // Variables
+
+    // Constructors
+
+    // Methods
+
+}
