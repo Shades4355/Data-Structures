389f1106dab83430b952c0e96bf7c6be56954fac
Shades <shades4355@gmail.com>
2024-07-28 18:13:08 -0400

    created an Entity parent for Enemy and Hero to manage shared methods
    * started fleshing out Entity, Enemy, and Hero

--- homework/assignment04/Enemy.java
+++ homework/assignment04/Enemy.java
@@ -2,19 +2,25 @@
 // Written by:  Shades Meyers
 // Description: A template for Enemies
 // Challenges:  
-// Time Spent:  1 minutes + ( - )
+// Time Spent:  28 minutes + ( - )
 //
 // Revision history:
 // Date:        By:     Action:
 // -------------------------------
 // 2024-July-27 SM      File created
+// 2024-July-28 SM      Started fleshing out class
+//                      Moved most methods to a parent class
+//                          (Entity.java)
 
 
-public class Enemy {
-    // Variables
+public class Enemy extends Entity{
+    // Variables (see Entity)
 
     // Constructors
+    Enemy(String name, int level, int hitDice, int damageDie, int armor, int str, int dex, int con) {
+        super(name, level, hitDice, damageDie, armor, str, dex, con);
+    }
 
     // Methods
-
-}
+    
+} // End class
--- homework/assignment04/Hero.java
+++ homework/assignment04/Hero.java
@@ -2,19 +2,67 @@
 // Written by:  Shades Meyers
 // Description: A template for the Player
 // Challenges:  
-// Time Spent:  1 minutes + ( - )
+// Time Spent:  28 minutes + ( - )
 //
 // Revision history:
 // Date:        By:     Action:
 // -------------------------------
 // 2024-July-27 SM      File created
+// 2024-July-28 SM      Started fleshing out class
+//                      Inspiration taken from my previous work
+//                          https://github.com/Shades4355/D20_Combat
 
 
-public class Hero {
+import java.util.Scanner;
+
+public class Hero extends Entity {
     // Variables
+    private Tree<String, Integer> inventory;
+    private int xp;
 
     // Constructors
+    Hero(String name) {
+        super(name, 1, 10, 4, 4, 1, 2, 2);
+        this.inventory = new Tree<String, Integer>(new Pairs<String, Integer>("Bombs", 2));
+        this.inventory.add("Bow", 1);
+        this.inventory.add("Arrows", 5);
+        this.inventory.add("Healing Potions", 3);
+        this.xp = 0;
+    }
 
     // Methods
+    // Accessors & Mutators
+        // XP
+    public int getXP() { return this.xp; }
+        // Inventory
+    public void getInventory() { System.out.println(inventory); }
+    public void checkInventory() {
+        Scanner input = new Scanner(System.in);
+        int invCap = 7 + this.str;
 
+        while (invCap < this.inventory.size()) {
+            System.out.println("\nYour bag has grown too heavy; pick an item to discard:");
+            this.inventory.forEach((n) -> System.out.println(n));
+
+            String choice = "";
+            while (!inventory.contains(choice)) {
+                System.out.print(">> ");
+                choice = input.nextLine();
+            }
+            inventory.remove(choice);
+        } 
+        input.close();
+    }
+
+    // Combat
+        // Gain XP
+    public void gainXP(int xpGained) {
+        this.xp += xpGained;
+
+        int threshold = 7 + this.level;
+        while (this.xp >= threshold) {
+            this.level += 1;
+            this.xp -= threshold;
+        }
+    }
 }
