5b9e33999ddf56de4c464a1cb4296f8d16f67a31
Shades <shades4355@gmail.com>
2024-07-29 00:20:34 -0400

    Enemy at MVP stage; possibly complete

--- homework/assignment04/Enemy.java
+++ homework/assignment04/Enemy.java
@@ -2,7 +2,7 @@
 // Written by:  Shades Meyers
 // Description: A template for Enemies
 // Challenges:  
-// Time Spent:  28 minutes + ( - )
+// Time Spent:  32 minutes
 //
 // Revision history:
 // Date:        By:     Action:
@@ -15,12 +15,15 @@
 
 public class Enemy extends Entity{
     // Variables (see Entity)
+    private int grantXP;
 
     // Constructors
-    Enemy(String name, int level, int hitDice, int damageDie, int armor, int str, int dex, int con) {
+    Enemy(String name, int level, int hitDice, int damageDie, int armor, int str, int dex, int con, int grantXP) {
         super(name, level, hitDice, damageDie, armor, str, dex, con);
+        this.grantXP = grantXP;
     }
 
     // Methods
+    public int grantXP() { return this.grantXP; }
     
 } // End class
