c83c752125956c0e9a92e86f87b7b60a69bcc37e
Shades <shades4355@gmail.com>
2024-07-31 13:59:45 -0400

    modified previous DLLNode to work with Map

--- homework/assignment04/DLLNode.java
+++ homework/assignment04/DLLNode.java
@@ -1,35 +1,48 @@
 // File name:   DLLNode.java
 // Written by:  Shades Meyers
-// Description: A Node for a DLL
+// Description: A node for a DLL
 // Challenges:  
-// Time Spent:  0 minutes
+// Time Spent:  3 minutes
 //
 // Revision history:
 // Date:        By:     Action:
 // -------------------------------
 // 2024-July-31 SM      File copied from in-class work
+//                      Modified to work with a Map
 
 
-public class DLLNode<E> {
+public class DLLNode<E extends Comparable<E>, T> {
     // variables
-    private E element;
-    private DLLNode<E> nextNode;
-    private DLLNode<E> prevNode;
+    private Pairs <E, T> element;
+    private DLLNode<E, T> nextNode;
+    private DLLNode<E, T> prevNode;
 
     // Constructor
-    DLLNode(E element) {
+    DLLNode() {
+        this.element = null;
+        this.nextNode = null;
+        this.prevNode = null;
+    }
+    DLLNode(Pairs<E, T> element) {
         this.element = element;
+        this.nextNode = null;
+        this.prevNode = null;
+    }
+    DLLNode(Pairs<E, T> element, DLLNode<E, T> prevNode, DLLNode<E, T> nextNode) {
+        this.element = element;
+        this.nextNode = nextNode;
+        this.prevNode = prevNode;
     }
 
     // Accessors & Mutators
-    public E getElement() { return this.element; }
-    public void setElement(E element) { this.element = element; }
+    public Pairs<E, T> getElement() { return this.element; }
+    public void setElement(Pairs<E, T> element) { this.element = element; }
 
-    public DLLNode<E> getNextNode() { return this.nextNode; }
-    public void setNextNode(DLLNode<E> nextNode) { this.nextNode = nextNode; }
+    public DLLNode<E, T> getNextNode() { return this.nextNode; }
+    public void setNextNode(DLLNode<E, T> nextNode) { this.nextNode = nextNode; }
 
-    public DLLNode<E> getPrevNode() { return this.prevNode; }
-    public void setPrevNode(DLLNode<E> prevNode) {
+    public DLLNode<E, T> getPrevNode() { return this.prevNode; }
+    public void setPrevNode(DLLNode<E, T> prevNode) {
         this.prevNode = prevNode;
     }
 
