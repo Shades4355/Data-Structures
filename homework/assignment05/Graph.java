1e493c82bbf5161f1941e7f41c0184748a73e9a1
Shades <shades4355@gmail.com>
2024-08-07 14:02:47 -0400

    outline started

--- homework/assignment05/Graph.java
+++ homework/assignment05/Graph.java
@@ -1,10 +1,30 @@
+// File name:   Graph.java
+// Written by:  Shades Meyers
+// Description: A Graph template
+// Challenges:  
+// Time Spent:  5 minutes
+// Revision history:
+// Date:        By:     Action:
+// -------------------------------
+// 2024-Aug-07  SM      File created
+
+
+import java.util.ArrayList;
+
 public class Graph {
+    // adjacency list style undirected Graph
     // variables
+    private ArrayList<Node> VertList = new ArrayList<Node>();
 
     // Constructors
+    Graph() {
+    }
+    Graph(Node vert) {
+        VertList.add(vert);
+    }
 
     // Methods
-    // numVertices()
+    public int numVertices() { return VertList.size(); }
     // vertices()
     // numEdges()
     // edges()
---
+++ homework/assignment05/Node.java
@@ -0,0 +1,14 @@
+// File name:   Node.java
+// Written by:  Shades Meyers
+// Description: A Node class for the Graph template
+// Challenges:  
+// Time Spent:  0 minutes
+// Revision history:
+// Date:        By:     Action:
+// -------------------------------
+// 2024-Aug-07  SM      File created
+
+
+public class Node {
+    
+}
