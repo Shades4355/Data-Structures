48bbc3fe192a22d753394ed248119e4107037c0b
Shades <shades4355@gmail.com>
2024-08-07 19:12:12 -0400

    minimally fleshed out Node.java such that Graph can call what needs to be called

--- homework/assignment05/Node.java
+++ homework/assignment05/Node.java
@@ -2,13 +2,26 @@
 // Written by:  Shades Meyers
 // Description: A Node class for the Graph template
 // Challenges:  
-// Time Spent:  0 minutes
+// Time Spent:  ~5 minutes
 // Revision history:
 // Date:        By:     Action:
 // -------------------------------
 // 2024-Aug-07  SM      File created
 
+import java.util.ArrayList;
 
-public class Node {
-    
+public class Node<E, T> {
+    // variables
+    E element;
+    ArrayList<Edge<T, E>> edges = new ArrayList<Edge<T, E>>();
+
+    // Constructors
+    Node(E element) {
+        this.element = element;
+    }
+
+    // Methods
+    public ArrayList<Edge<T, E>> getEdges() { return this.edges; }
+    public void addEdge( Edge<T,E> newEdge) { this.edges.add(newEdge); }
+    public void removeEdge(Edge<T,E> edge) { this.edges.remove(edge); }
 }
