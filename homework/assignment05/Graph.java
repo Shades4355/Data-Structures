2e0d531b6b1bfb070b08c11d89c8f05fd5876caf
Shades <shades4355@gmail.com>
2024-08-07 13:32:24 -0400

    undirected Graph template outline

---
+++ homework/assignment05/Graph.java
@@ -0,0 +1,18 @@
+public class Graph {
+    // variables
+
+    // Constructors
+
+    // Methods
+    // numVertices()
+    // vertices()
+    // numEdges()
+    // edges()
+    // getEdge(vert1, vert2)
+    // endVertices(edge)
+    // opposite(vert, edge)
+    // insertVertex(element)
+    // insertEdge(vert1, vert2, element)
+    // removeVertex(vertex)
+    // removeEdge(element)
+}
